aa0ef8dac8904edfdfc6ff15f0a78fb365726927
Lucas Acosta <lacosta@makingsense.com>
2015-08-20 15:59:14 -0300

    Changed recipe to be manual instead of relying in 'package'

--- codedeploy_agent/recipes/install.rb
+++ codedeploy_agent/recipes/install.rb
@@ -1,4 +1,12 @@
-package 'codedeploy-agent' do
+include_recipe 'awscli'
+
+remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb" do
+  source 'https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/codedeploy-agent_all.deb'
+  action :create
+end
+
+dpkg_package 'codedeploy-agent' do
+  source "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb"
   action :install
 end
 
