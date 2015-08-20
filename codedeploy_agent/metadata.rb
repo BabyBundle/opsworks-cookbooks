cf1535609e8ac3d5f911c3a60028893052592468
Lucas Acosta <lacosta@makingsense.com>
2015-08-20 16:05:28 -0300

    Removed aws-cli dependency

--- codedeploy_agent/metadata.rb
+++ codedeploy_agent/metadata.rb
@@ -1,6 +1,4 @@
 name        "codedeploy_agent"
 description "Installs and configures CodeDeploy agent"
 
-depends 'awscli'
-
 recipe 'codedeploy_agent::install', "Installs CodeDeploy agent"
--- codedeploy_agent/recipes/install.rb
+++ codedeploy_agent/recipes/install.rb
@@ -1,5 +1,3 @@
-include_recipe 'awscli'
-
 remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb" do
   source 'https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/codedeploy-agent_all.deb'
   action :create
