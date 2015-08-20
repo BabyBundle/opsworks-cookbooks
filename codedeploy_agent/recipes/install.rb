6fab8c95baf26551ed487e6e96095ee3209599f0
Lucas Acosta <lacosta@makingsense.com>
2015-08-20 18:48:51 -0300

    Added install of package dependencies

--- codedeploy_agent/recipes/install.rb
+++ codedeploy_agent/recipes/install.rb
@@ -1,3 +1,11 @@
+package 'ruby2.0' do
+  :install
+end
+
+package 'awscli' do
+  :install
+end
+
 remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb" do
   source 'https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/codedeploy-agent_all.deb'
   action :create
