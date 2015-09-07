aa43f9007f2e2d797ca6e34e80a656e7e059be8a
Lucas Acosta <lacosta@makingsense.com>
2015-09-07 16:06:08 -0300

    Changed execute block into bash

--- mongodb_client/recipes/install.rb
+++ mongodb_client/recipes/install.rb
@@ -11,13 +11,13 @@ node[:deploy].each do |application, deploy|
       group "root"
   end
 
-  execute "install_mongodb_client" do
-    command "tar zxvf #{Chef::Config[:file_cache_path]}/mongodb-linux-x86_64-3.0.4.tgz -C #{Chef::Config[:file_cache_path]}/"
-    user "root"
+  bash 'install_mongodb_and_run_script' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOF
+      source #{deploy[:deploy_to]}/shared/app.env
+      tar zxvf mongodb-linux-x86_64-3.0.4.tgz
+      ./mongodb-linux-x86_64-3.0.4/bin/mongo $DB_HOST/$DB_NAME -u $DB_USER -p $DB_PASSWORD < #{deploy[:deploy_to]}/current/scripts/cleanAndSeedDb.js
+    EOF
   end
 
-  execute "clear_database" do
-    command "source #{deploy[:deploy_to]}/shared/app.env && #{Chef::Config[:file_cache_path]}/mongodb-linux-x86_64-3.0.4/bin/mongo $DB_HOST/$DB_NAME -u $DB_USER -p $DB_PASSWORD < #{deploy[:deploy_to]}/current/scripts/cleanAndSeedDb.js"
-    user "root"
-  end
 end
